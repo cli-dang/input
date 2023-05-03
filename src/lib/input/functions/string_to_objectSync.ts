a81d90f76f2646f52dda33be9ba913a0a71395c2
Simone Del Popolo <simonedelpopolo@outlook.com>
2023-05-03 10:18:22 +0000

    input v4.0.0
    refactoring Types

--- src/lib/input/functions/string_to_objectSync.ts
+++ src/lib/input/functions/string_to_objectSync.ts
@@ -1,27 +1,31 @@
-export function string_to_objectSync( pattern: string, single: boolean ):string|OptionsType{
+export function string_to_objectSync( pattern: string, single: boolean ): string | Input.OptionsType {
 
-  const scalpel = single ? ':' : '|'
+  const scalpel: string = single
+    ? ':'
+    : '|'
 
-  const option_value_reg_expression = new RegExp( `(.*)[${scalpel}](.*)`, 'g' )
-  const body_array_expression = Array.from( pattern.matchAll( option_value_reg_expression ), body_value_matches => body_value_matches[ 0 ] )
+  const option_value_reg_expression: RegExp = new RegExp( `(.*)[${scalpel}](.*)`, 'g' )
+  const body_array_expression: string[] = Array.from( pattern.matchAll( option_value_reg_expression ), ( body_value_matches: RegExpMatchArray ) => body_value_matches[ 0 ] )
 
   if ( typeof body_array_expression[ 0 ] === 'undefined' )
 
     return pattern
 
-  const body_array = body_array_expression[ 0 ].replaceAll( scalpel, ':' )
+  const body_array: string[] = body_array_expression[ 0 ].replaceAll( scalpel, ':' )
     .split( ':' )
   if ( body_array.length % 2 !== 0 )
     return pattern
 
-  const even = 0
-  const property_value = []
+  const even: number = 0
+  const property_value: Array<string[]> = []
 
   for ( const key in body_array ) {
 
     if ( even + parseInt( key ) % 2 === 0 ) {
-      let value_ = <string>body_array[ parseInt( key ) + 1 ]
-      value_ = value_.length === 0 ? undefined : value_
+      let value_: string = <string> body_array[ parseInt( key ) + 1 ]
+      value_ = value_.length === 0
+        ? undefined
+        : value_
       property_value.push( Array.of( body_array[ parseInt( key ) ], value_ ) )
     }
   }
