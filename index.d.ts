257fb7530b1e09c6502764cc8d88d2727eaad9ad
Simone Del Popolo <simonedelpopolo@outlook.com>
2023-05-06 13:21:30 +0000

    input v4.0.0
    - fixing Types

--- index.d.ts
+++ index.d.ts
@@ -1,97 +1,137 @@
-export function entry_point( argv: string[], logic: LogicParameter ): Promise<void>;
-export function options( pattern: string, reference_to_flag: string ): Promise<Error | OptionsType>;
-export function processor( argv: string[] ): Promise<ParsedArgv>;
+import { OftypesError } from "oftypes"
+
+export declare function entry_point( argv: NodeJS.Process['argv'], logic: Input.LogicParameter ): Promise<Error | undefined>;
+
+export declare function options( pattern: string, reference_to_flag: string ): Promise<OftypesError | Input.OptionsType>;
+
+export declare function processor( argv: string[] ): Promise<Input.ParsedArgv | OftypesError>;
 
 declare global {
-  type OptionsType = {
-    [p:string]:string
-  }
-  type ParsedArgv = { object: { [ p: string ]: unknown }, keys: string[], flag?: object|string, command?:string, flag_returns?: unknown}
-  type RestArgsCallbacks = Array<boolean | string | object | number>
-  type LogicParameter = ( data: ParsedArgv ) => Promise<void>
 
-  type FlagType = 'string' | 'boolean' | 'number' | 'opts' | 'json' | 'buf' | 'null'
-  type GlobalFlagType = 'string' | 'opts' | 'object'
-  type FlagDescriptor ={
-    short: string
-    priority?: number
-    long?: string | null
-    conflict?: string[] | null
-    description?: string | null
-    usage?: string | null
-    void?: boolean | null
-    type?: FlagType | null
-    check?: boolean | null
-    cb?: FlagsCallBack | null,
-    rest_args?: RestArgsCallbacks
-  }
-  type FlagsCallBack =
-    ( <cb>( data: cb, ...rest_args: RestArgsCallbacks ) => Promise<void> | void | Promise<cb> | cb )
-    | null
+    namespace Input {
+        type OptionsType = {
+            [p: string]: string
+        }
 
-  type CommandCallBack =
-    ( <cb>( data: cb, ...rest_args: RestArgsCallbacks ) => Promise<void> | void | Promise<cb> | cb )
-    | null
-  type CommandsDefinition = {
-    [ name: string ]: {
-      flags?: { [ name: string ]: FlagDescriptor } | null,
-      cb: CommandCallBack,
-      rest_args?: RestArgsCallbacks,
-      description: string,
-      usage: string
+        interface expectedStringProperty {
+            [p: string]: string
+        }
+
+        interface expectedOptionsTypeProperty {
+            [p: string]: Input.OptionsType
+        }
+
+        type ARGVtoObject<T> = T extends Input.expectedOptionsTypeProperty & Input.expectedStringProperty ? Input.OptionsType : ParsedArgv
+
+        type ParsedArgv = {
+            object: { [p: string]: unknown },
+            keys: string[],
+            flag?: object | string,
+            command?: string,
+            flag_returns?: unknown
+        }
+        type RestArgsCallbacks = Array<boolean | string | object | number>
+        type LogicParameter = ( data: ParsedArgv ) => Promise<Error | void>
+
+        interface Help {
+            description: string | null,
+            usage: string | null
+        }
+
+        type FlagType = 'string' | 'boolean' | 'number' | 'opts' | 'json' | 'buf' | 'null'
+        type GlobalFlagType = 'string' | 'boolean' | 'number' | 'opts' | 'json' | 'buf' | 'null'
+        type FlagDescriptor = {
+            short: string
+            priority?: number
+            long?: string | null
+            conflict?: string[] | null
+            description?: Help['description']
+            usage?: Help['usage']
+            void?: boolean | null
+            type?: FlagType | null
+            check?: boolean | null
+            cb?: FlagsCallBack,
+            rest_args?: RestArgsCallbacks
+        }
+        type FlagsCallBack =
+            ( <cb>( data: cb, ...rest_args: RestArgsCallbacks ) => Promise<void> | void | Promise<cb> | cb )
+            | null
+
+        type CommandCallBack =
+            ( <cb>( data: cb | Input.ParsedArgv, ...rest_args: RestArgsCallbacks ) => Promise<void> | void | Promise<cb> | cb )
+            | null
+        type CommandsDefinition = {
+            [command_name: string]: {
+                flags?: { [flag_name: string]: FlagDescriptor } | null,
+                cb: CommandCallBack,
+                rest_args?: RestArgsCallbacks,
+                description: string,
+                usage: string
+            }
+        }
+        type GlobalFlag = {
+            [name: string]: {
+                cb?: CommandCallBack,
+                rest_args?: RestArgsCallbacks,
+                type?: GlobalFlagType,
+                description?: string,
+                usage?: string
+            }
+        }
+        type checkoutCommand = {
+            flags?: {
+                [p: string]: FlagDescriptor
+            }
+        }
+
+        type checkoutGlobal = {
+            cb?: CommandCallBack,
+            rest_args?: RestArgsCallbacks,
+            type?: GlobalFlagType,
+            description?: string,
+            usage?: string
+        }
+
+        type executor = {
+            flag_cb: FlagsCallBack,
+            arg: string | object,
+            rest_args_cb: RestArgsCallbacks,
+        }
+
+        type executor_flag_cb = {
+            priority_group: {
+                '0': { [flag: string]: executor },
+                '1': { [flag: string]: executor },
+                '-1': { [flag: string]: executor }
+            }
+        }
+
+
+        interface InterfaceCommand {
+            checkout( name?: string | undefined ): Input.checkoutCommand | Input.CommandsDefinition;
+
+            checkout_global( name?: string | undefined ): Input.checkoutGlobal | Input.GlobalFlag;
+
+            intercept(): Promise<void>;
+
+            define( name: string, cb: Input.CommandCallBack, info?: Input.Help, global?: boolean, global_type?: Input.GlobalFlagType, rest_args?: Input.RestArgsCallbacks ): Promise<ReferenceError | void>;
+
+            flag( name: string | string[], descriptor: Input.FlagDescriptor ): Promise<void>;
+        }
     }
-  }
-  type GlobalFlag = {
-      [ name: string ]: {
-        cb?: CommandCallBack,
-        rest_args?: RestArgsCallbacks,
-        type?: GlobalFlagType,
-        description?: string,
-        usage?: string
-      }
-  }
-  type checkoutCommand = {
-    flags?: {
-      [ p: string ]: FlagDescriptor
-    }
-  }
 
-  type checkoutGlobal = {
-    cb?: CommandCallBack,
-    rest_args?: RestArgsCallbacks,
-    type?: GlobalFlagType,
-    description?: string,
-    usage?: string
-  }
-
-  type executor = {
-    flag_cb: FlagsCallBack,
-    arg: string | object,
-    rest_args_cb: RestArgsCallbacks,
-  }
-
-  type executor_flag_cb = {
-    priority_group: {
-      '0': { [flag: string]: executor },
-      '1': { [flag: string]: executor },
-      '-1': { [flag:string]: executor }
-    }
-  }
-
-
-  interface InterfaceCommand {
-    checkout( name?: string | undefined ): checkoutCommand | CommandsDefinition
-    checkout_global( name?: string | undefined ): checkoutGlobal | GlobalFlag
-    intercept( parsed:ParsedArgv ):Promise<void>
-    define: ( name: string, cb: CommandCallBack, info?: {description:string, usage:string}, global?:boolean, global_type?:GlobalFlagType, rest_args?:RestArgsCallbacks ) => void
-    flag: ( name: string|string[], descriptor: FlagDescriptor ) => Promise<void>
-  }
 }
 
-export class Command implements InterfaceCommand {
-  checkout( name?: string | undefined ): checkoutCommand | CommandsDefinition
-  checkout_global( name?: string | undefined ): checkoutGlobal | GlobalFlag
-  intercept( parsed:ParsedArgv ): Promise<void>
-  define: ( name: string, cb: CommandCallBack, info?: {description:string, usage:string}, global?:boolean, global_type?:GlobalFlagType, rest_args?:RestArgsCallbacks ) => void
-  flag: ( name: string|string[], descriptor: FlagDescriptor ) => Promise<void>
+export declare class Command implements Input.InterfaceCommand {
+  constructor( parsedARGV: Input.ParsedArgv, v_per_version?: boolean );
+
+  checkout( name?: string | undefined ): Input.checkoutCommand | Input.CommandsDefinition;
+
+  checkout_global( name?: string | undefined ): Input.checkoutGlobal | Input.GlobalFlag;
+
+  intercept(): Promise<void>;
+
+  define( name: string, cb: Input.CommandCallBack, info?: Input.Help, global?: boolean, global_type?: Input.GlobalFlagType, rest_args?: Input.RestArgsCallbacks ): Promise<ReferenceError | void>;
+
+  flag( name: string | string[], descriptor: Input.FlagDescriptor ): Promise<void>;
 }
